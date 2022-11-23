90be4b715e9a407bf5671f91d42990a465310da8
rpahel <raphael.dacosta.pro@gmail.com>
2022-11-23 22:26:30 +0100

    Block trap moves and can be dynamically set up in editor

--- Assets/Scripts/Gow_Trap_Block.cs
+++ Assets/Scripts/Gow_Trap_Block.cs
@@ -1,10 +1,133 @@
+using System.Collections;
 using UnityEngine;
 
+[ExecuteInEditMode]
 public class Gow_Trap_Block : MonoBehaviour
 {
     public float start_position;
     public float end_position;
     public float time_to_start;
     public float time_between_moves;
+
+    //================================================================================
+    private Vector3 initial_localposition;
+    public Vector3 InitialLocalPosition { set { initial_localposition = value; } }
     private float move_duration;
+    public float MoveDuration {  set { move_duration = value; } }
+    private float half_block_height;
+    private AnimationCurve curve;
+    public AnimationCurve Curve { set { curve = value;} }
+    private bool start_to_end;
+    private bool frozen;
+
+    //================================================================================
+    private Vector3 start_localposition_v3;
+    private Vector3 end_localposition_v3;
+
+    private void Start()
+    {
+        if (!Application.isPlaying)
+            return;
+
+        start_localposition_v3 = initial_localposition + start_position * Vector3.forward;
+        end_localposition_v3 = initial_localposition - end_position * Vector3.forward;
+        transform.localPosition = start_localposition_v3;
+        StartCoroutine(MoveCoroutine());
+    }
+
+    IEnumerator MoveCoroutine(bool go_back = false)
+    {
+        yield return new WaitForSeconds(time_to_start);
+
+        float t = 0;
+        while (true)
+        {
+            if (!go_back)
+            {
+                start_to_end = true;
+                while(t < 1)
+                {
+                    if (frozen)
+                        break;
+
+                    transform.localPosition = Vector3.LerpUnclamped(start_localposition_v3, end_localposition_v3, curve.Evaluate(t));
+                    t += Time.fixedDeltaTime / move_duration;
+                    yield return new WaitForFixedUpdate();
+                }
+
+                if (frozen)
+                    break;
+
+                transform.localPosition = end_localposition_v3;
+                go_back = true;
+            }
+            else
+            {
+                start_to_end = false;
+                while (t < 1)
+                {
+                    if (frozen)
+                        break;
+
+                    transform.localPosition = Vector3.LerpUnclamped(end_localposition_v3, start_localposition_v3, curve.Evaluate(t));
+                    t += Time.fixedDeltaTime / move_duration;
+                    yield return new WaitForFixedUpdate();
+                }
+
+                if (frozen)
+                    break;
+
+                transform.localPosition = start_localposition_v3;
+                go_back = false;
+            }
+
+            t = 0;
+            while(t < time_between_moves)
+            {
+                if (frozen)
+                    break;
+
+                t += Time.fixedDeltaTime;
+                yield return new WaitForFixedUpdate();
+            }
+
+            t = 0;
+        }
+
+        //if (frozen) Initiate slowing down coroutine;
+    }
+
+    public void ApplyProperties(BlockSettings block_settings)
+    {
+        start_position = block_settings.start_position;
+        end_position = block_settings.end_position;
+        time_to_start = block_settings.time_to_start;
+        time_between_moves = block_settings.time_between_moves;
+    }
+
+#if UNITY_EDITOR
+    private void Update()
+    {
+        if (!Application.isPlaying)
+        {
+            transform.localPosition = initial_localposition + start_position * Vector3.forward;
+            return;
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+        half_block_height = transform.localScale.y * .5f;
+
+        Gizmos.color = Color.cyan;
+        Gizmos.DrawLine((transform.parent.position + initial_localposition) + half_block_height * Vector3.up,
+                        (transform.parent.position + initial_localposition) + half_block_height * Vector3.up + start_position * Vector3.forward);
+        Gizmos.DrawWireCube((transform.parent.position + initial_localposition) + half_block_height * Vector3.up + start_position * Vector3.forward, transform.localScale);
+
+        Gizmos.color = Color.magenta;
+        Gizmos.DrawLine((transform.parent.position + initial_localposition) + half_block_height * Vector3.up,
+                        (transform.parent.position + initial_localposition) + half_block_height * Vector3.up - end_position * Vector3.forward);
+        Gizmos.DrawWireCube((transform.parent.position + initial_localposition) + half_block_height * Vector3.up - end_position * Vector3.forward, transform.localScale);
+    }
+#endif
 }
--- Assets/Scripts/Gow_Trap_Blocks.cs
+++ Assets/Scripts/Gow_Trap_Blocks.cs
@@ -1,26 +1,51 @@
+using System;
 using UnityEngine;
 
+[ExecuteInEditMode]
 public class Gow_Trap_Blocks : MonoBehaviour
 {
+    [Header("Blocks move")]
+    [SerializeField] private float move_duration;
+    [SerializeField] private AnimationCurve move_ease;
+
+    [Space(10)]
+    [Header("General block properties")]
+    public float start_position;
+    public float end_position;
+    public float time_to_start;
+    public float time_between_moves;
+
+    [Space(10)]
     [Header("Block")]
     [SerializeField] private GameObject block_prefab;
     private float block_width;
     private Gow_Trap_Block[] blocks;
 
-    [Header("Blocks move")]
-    [SerializeField] private float move_duration;
-    [SerializeField] private AnimationCurve move_ease;
+    [Space(10)]
+    [Header("Block Properties")]
+    public BlockSettings[] block_settings;
 
-    public void Start()
+    private void Awake()
     {
-        CreateTrapBlocks(5);
+        if (Application.isPlaying)
+        {
+            CreateTrapBlocks(block_settings.Length);
+        }
+    }
+
+    private void Update()
+    {
+        if (!Application.isPlaying)
+        {
+            CreateTrapBlocks(block_settings.Length);
+        }
     }
 
     /// <summary>
     /// Creates the given number of blocks for the trap.
     /// </summary>
     /// <param name="n">The number of blocks to be created.</param>
-    public void CreateTrapBlocks(int n = 3)
+    private void CreateTrapBlocks(int n = 3)
     {
         DestroyTrapBlocks();
 
@@ -31,8 +56,13 @@ public class Gow_Trap_Blocks : MonoBehaviour
 
         for(int i = 0; i < n; i++)
         {
-            blocks[i] = Instantiate(block_prefab, new Vector3(block_width * (i - 0.5f * (n-1)), 0, 0), Quaternion.identity, transform).GetComponent<Gow_Trap_Block>();
+            blocks[i] = Instantiate(block_prefab, Vector3.zero, Quaternion.identity, transform).GetComponent<Gow_Trap_Block>();
+            blocks[i].transform.localPosition = new Vector3(block_width * (i - 0.5f * (n - 1)), 0, 0);
+            blocks[i].InitialLocalPosition = blocks[i].transform.localPosition;
             blocks[i].gameObject.name = "Block " + i.ToString("00");
+            blocks[i].ApplyProperties(block_settings[i]);
+            blocks[i].MoveDuration = move_duration;
+            blocks[i].Curve = move_ease;
         }
     }
 
@@ -48,4 +78,25 @@ public class Gow_Trap_Blocks : MonoBehaviour
 
         blocks = null;
     }
+
+    [ContextMenu("Apply all general settings.")]
+    public void ApplyAllGeneralProperties()
+    {
+        for (int i = 0; i < blocks.Length; i++)
+        {
+            block_settings[i].start_position = start_position;
+            block_settings[i].end_position = end_position;
+            block_settings[i].time_to_start = time_to_start;
+            block_settings[i].time_between_moves = time_between_moves;
+        }
+    }
+}
+
+[Serializable]
+public struct BlockSettings
+{
+    public float start_position;
+    public float end_position;
+    public float time_to_start;
+    public float time_between_moves;
 }
